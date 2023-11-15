62e5352a09375ec8466ece644f51f51862d85d80
Eric <ericl160@nycstudents.net>
2023-11-15 11:33:27 -0500

    Finished

--- Algorithms.java
+++ Algorithms.java
@@ -2,7 +2,36 @@ public class Algorithms
 {
     public boolean containsString(String a, String b)
     {
-        if (a.contains(b)) return true;
-        return false;
+        return a.contains(b);
     }
+    public String reverseString(String str)
+    {
+        String result = "";
+        for(int i = str.length(); i>0; i--)
+        {
+            result += str.substring(i-1, i);
+        }
+        return result;
+    }
+    public boolean isPalindrome(String str)
+    {
+        return str.equals(reverseString(str));
+    }
+    public int numOfOccurrences(String a, String b)
+    {
+        int result = 0;
+        if (b.length() > a.length())
+        {
+            return result;
+        }
+        for(int i = b.length(); i <= a.length(); i++)
+        {
+            if (a.substring(i-b.length(), i).equals(b))
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,18 @@ public class Main
 {
     public static void main(String[] args)
     {
+        Algorithms a = new Algorithms();
+        System.out.println("containsString");
+        System.out.println(a.containsString("antidisestablishmentarianism", "establish"));
+        System.out.println(a.containsString("Bermuda", "Spain"));
+        System.out.println("reverseString");
+        System.out.println(a.reverseString("happy"));
+        System.out.println(a.reverseString("juxtaposition"));
+        System.out.println(a.isPalindrome("racecar"));
+        System.out.println(a.isPalindrome("waffles"));
+        System.out.println("numOfOccurrences:");
+        System.out.println(a.numOfOccurrences("Mississippi", "si"));
+        System.out.println(a.numOfOccurrences("Banana", "ana"));
 
     }
 }
